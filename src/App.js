18720c18c8051ea7edf0bf0714b0f7a8e852dacd
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2021-05-19 13:45:10 -0400

    semaforoTerminado

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="traffic">
+      <div className="post"></div>
+      <div className="body">
+        <div className="red"></div>
+        <div className="yellow"></div>
+        <div className="green"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

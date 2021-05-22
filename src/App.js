fd4be4b4480e694b107caaf5362a165df0784a8f
Gabriel Lillo <gabriel.lillovjm@gmail.com>
2021-05-22 19:59:19 -0400

    TrfficLight

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ colorRed, setColorRed ] = useState("");
+  const [ colorYellow, setColorYellow ] = useState("");
+  const [ colorGreen, setColorGreen ] = useState("");
+  
   return (
-    <div className="traffic">
+    <div className="trafficLight">
       <div className="post"></div>
       <div className="body">
-        <div className="red"></div>
-        <div className="yellow"></div>
-        <div className="green"></div>
+        <div className={"red" + colorRed } onClick={() => (
+          setColorRed(" selected"),
+          setColorYellow(""),
+          setColorGreen("")
+          )}
+        ></div>
+        <div className={"yellow" + colorYellow} onClick={() => (
+          setColorRed(""),
+          setColorYellow(" selected"),
+          setColorGreen("")
+          )}
+        ></div>
+        <div className={"green"+ colorGreen} onClick={() => (
+          setColorRed(""),
+          setColorYellow(""),
+          setColorGreen(" selected")
+          )}
+        ></div>
       </div>
     </div>
   );
